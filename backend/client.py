b08507d951d7ff4d1baa29470da8c93d5d385d14
Henry Ventura <hvent90@gmail.com>
2024-08-13 10:11:28 -0700

    Update client.py

--- backend/client.py
+++ backend/client.py
@@ -1,19 +1,3 @@
-# import asyncio
-# import sys
-# import os
-
-# # Add the directory containing the proto files to the Python path
-# import os
-# import sys
-
-
-# proto_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'proto_generated')
-# sys.path.insert(0, proto_dir)
-
-# # # Add the Backend directory to the Python path
-# backend_dir = os.path.dirname(os.path.dirname(__file__))
-# sys.path.insert(0, backend_dir)
-
 import asyncio
 from grpclib.client import Channel
 from proto import PingTestServiceStub, PingTestRequest
